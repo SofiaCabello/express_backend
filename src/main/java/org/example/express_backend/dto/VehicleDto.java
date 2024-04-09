b64b3f4f8f72d782af6026f6a3d849e6baa4dd6f
jintaixi <sofiacabello666@outlook.com>
2024-04-09 11:47:30 +0800

    修正了Point的数据类型。自定义了Point类。

--- src/main/java/org/example/express_backend/dto/VehicleDto.java
+++ src/main/java/org/example/express_backend/dto/VehicleDto.java
@@ -2,11 +2,12 @@ package org.example.express_backend.dto;
 
 import lombok.Data;
 import lombok.ToString;
+import org.example.express_backend.entity.Point;
 
 @Data
 @ToString
 public class VehicleDto {
     private Integer id;
 
-    private Integer coordinate;
+    private Point coordinate;
 }
---
+++ src/main/java/org/example/express_backend/entity/Point.java
@@ -0,0 +1,9 @@
+package org.example.express_backend.entity;
+
+import lombok.Data;
+
+@Data
+public class Point {
+    private Double latitude; // 经度
+    private Double longitude; // 纬度
+}
